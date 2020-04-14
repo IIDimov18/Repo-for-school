38d6700ff706164205958c0db6442f521396a779
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-04-14 21:03:54 +0300

    made structures that may need update

--- Repo-for-school/Repo-for-school.cpp
+++ Repo-for-school/Repo-for-school.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+﻿#include <iostream>
+#include <string>
 using namespace std;
+struct ITEMS
+{
+	string user;
+	string itemName;
+	float price;
+};
+struct USER
+{
+	string username;
+	string password;
+	int countOfItemsForSell;
+	ITEMS itemsForSale[];
+};
+int login() {
+	int choice;
+	string username, password;
+	cout << "You need to login. Choose role you want to login with." << endl << "1) User" << endl << "2) Admin" << endl << "Your choice(answer with number):";
+	cin >> choice;
+	cout << "Username: ";
+	cin >> username;
+	cout << "Password: ";
+	cin >> password;
+	switch (choice)
+	{
+	case 1:
+		
+		break;
+	case 2:
+		
+		break;
+	default:
+		break;
+	}
+}
 int main()
 {
-    cout << "Test commit";
-	cout << "kartof";
 }
