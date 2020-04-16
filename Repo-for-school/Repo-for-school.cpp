8a900682f54b51ea85d419858afa93324cb61f42
IDIliev18 <IDIliev18@codingburgas.bg>
2020-04-16 19:04:25 +0300

    Add id field in the structure and declare array of structures

--- Repo-for-school/Repo-for-school.cpp
+++ Repo-for-school/Repo-for-school.cpp
@@ -1,11 +1,14 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
+
+
 struct ITEMS
 {
 	string user;
 	string itemName;
 	float price;
+	int quantity;
 };
 struct USER
 {
@@ -14,7 +17,7 @@ struct USER
 	int countOfItemsForSell;
 	ITEMS itemsForSale[];
 };
-int login() {
+/*int login() {
 	int choice;
 	string username, password;
 	cout << "You need to login. Choose role you want to login with." << endl << "1) User" << endl << "2) Admin" << endl << "Your choice(answer with number):";
@@ -35,6 +38,9 @@ int login() {
 		break;
 	}
 }
+*/
+
 int main()
 {
+	cout << "TEST";
 }
