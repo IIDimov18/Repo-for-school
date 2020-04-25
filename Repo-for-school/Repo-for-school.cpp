9a3bfca5761840d71f4d84abb93da741cd55d47e
IDIliev18 <IDIliev18@codingburgas.bg>
2020-04-25 21:06:27 +0300

    Add function to write in file

--- Repo-for-school/Repo-for-school.cpp
+++ Repo-for-school/Repo-for-school.cpp
@@ -12,7 +12,6 @@ struct ITEM
 	string itemName = "";
 	float price = 0;
 	string description = "";
-	int id = 0;
 };
 
 struct USER
@@ -127,41 +126,98 @@ void Menu() {
 		}
 	}
 }
-void createItem(ITEM* items, int& orderCount, ITEM newItem, int& maxId)
-{
-	newItem.id = maxId;
-	items[orderCount] = newItem;
-
-	orderCount++;
-	maxId++;
-}
-
-int getItemIndexById(ITEM* items, int& itemCount, int id)
-{
-	for (int index = 0; index < itemCount; index++)
+void login() {
+	string username, password;
+	char character;
+	cout << "Do you have existing account[Y/N]: ";
+	cin >> character;
+	cout << endl;
+	if (character == 'Y')
 	{
-		if (items[index].id == id) // check 
+		cout << "___________________________________________________" << endl;
+		cout << endl;
+		cout << "               |===== LOGIN =====|\n\n" << endl;
+		cout << "Username: ";
+		cin >> username;
+		cout << "Password: ";
+		cin >> password;
+		cout << endl;
+		cout << checkAcc(username, password);
+	}
+	else
+	{
+
+	}
+}
+void Menu() {
+	char input;
+	bool whileCheck = true;
+
+	cout << "\n|============== Welcome to OLX_2.0 ==============|\n" << endl;
+	cout << endl;
+	cout << "                       Menu:\n" << endl;
+	cout << "                   1. Login" << endl;
+	cout << "                   2. Register" << endl;
+	cout << "                   3. Show Offers" << endl;
+	cout << "                   9. Exit\n\n";
+	cout << "\nChoose an option: ";
+	cin >> input;
+	cout << endl;
+
+	while (whileCheck)
+	{
+		switch (input)
 		{
-			return index;
+		case '1':
+			login();
+			whileCheck = false;
+			break;
+		case '2':
+			whileCheck = false;
+			break;
+		case '3':
+			whileCheck = false;
+		case '9':
+			whileCheck = false;
+			break;
+		default:
+			break;
 		}
 	}
-
-	return -1;
 }
-
-void initItems(ITEM* items, int& itemCount, int& maxId) 
+void createItem(ITEM* items, int& orderCount, ITEM newItem)
 {
-	createItem(items, itemCount, { "Gosho", "Bathroom tiles", 12.35, "the price is for m / sq" }, maxId);
-	createItem(items, itemCount, { "Alex", "Mouse Pad", 21.45, "35x45" }, maxId);
-	createItem(items, itemCount, { "Pesho", "LG TV", 769.99, "42 inches " }, maxId);
-	createItem(items, itemCount, { "Penka", "T-Shirt", 9.99, "XL size " }, maxId);
-	createItem(items, itemCount, { "Nelina", "White Mercedes", 6829, "Year of manufacture: 1997 " }, maxId);
-	createItem(items, itemCount, { "Milko", "Chickens", 20, "One chicken- 20 bgn " }, maxId);
-	createItem(items, itemCount, { "John", "Fridge", 178, "2x1" }, maxId);
-	createItem(items, itemCount, { "Miroslav", "Leather", 25, "25 bgn for 1 meter" }, maxId);
-	createItem(items, itemCount, { "Ivan", "Turkeys", 35, "35 bgn for 1 turkey" }, maxId);
-	createItem(items, itemCount, { "Martin", "Pillow", 15, "15 bgn for 1 pillow" }, maxId);
+	items[orderCount] = newItem;
+	orderCount++;	
 }
+
+
+
+void initItems(ITEM* items, int& itemCount) 
+{
+	createItem(items, itemCount, { "Gosho", "Bathroom tiles", 12.35, "the price is for m / sq" });
+	createItem(items, itemCount, { "Alex", "Mouse Pad", 21.45, "35x45" });
+	createItem(items, itemCount, { "Pesho", "LG TV", 769.99, "42 inches"});
+	createItem(items, itemCount, { "Penka", "T-Shirt", 9.99, "XL size "});
+	createItem(items, itemCount, { "Nelina", "White Mercedes", 6829, "Year of manufacture: 1997 "});
+	createItem(items, itemCount, { "Milko", "Chickens", 20, "One chicken- 20 bgn "});
+	createItem(items, itemCount, { "John", "Fridge", 178, "2x1" });
+	createItem(items, itemCount, { "Miroslav", "Leather", 25, "25 bgn for 1 meter" });
+	createItem(items, itemCount, { "Ivan", "Turkeys", 35, "35 bgn for 1 turkey" });
+	createItem(items, itemCount, { "Martin", "Pillow", 15, "15 bgn for 1 pillow" });
+}
+
+void writeInFile(ITEM* item, int itemCount)
+{
+	ofstream data;
+	data.open("data.txt");
+	for (int i = 0; i < itemCount; i++)
+	{
+		data << item[i].itemName << "|" << item[i].seller << "|" << item[i].price << "|" << item[i].description << endl;
+	}
+	data.close();
+}
+
 //void split(char character, string& arr,string stringToSplit) {
 //	char help[30];
 //	int counter = 0, arrCounter = 0;;
@@ -181,8 +237,9 @@ void initItems(ITEM* items, int& itemCount, int& maxId)
 int main()
 {
 	int itemCount = 0;
-	int maxId = 1;
 	ITEM items[200];
+	initItems(items, itemCount);
+	writeInFile(items, itemCount);
 	/*string line;
 	ifstream myfile("data.txt");
 	if (myfile.is_open())
@@ -194,6 +251,6 @@ int main()
 		}
 		myfile.close();
 	}*/
-	Menu();
+	//Menu();
 	return 0;
 }
