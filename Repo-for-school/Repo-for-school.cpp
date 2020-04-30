79f775e0dd1ff7e7492044969f7d82f38a4d69a3
Ivan_Dimov <IIDimov18@codingburgas.bg>
2020-04-30 21:51:43 +0300

    Added Deleting account, and adding/removing admin

--- Repo-for-school/Repo-for-school.cpp
+++ Repo-for-school/Repo-for-school.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <stdio.h>
 #include <string.h>
+#include <windows.h>
 #include "Structures.h"
 
 using namespace std;
@@ -29,7 +30,7 @@ string checkAcc(string username, string password)
 				}
 				else
 				{
-					cout << "Invalid account!" << endl;
+					cout << "Invalid password or username!" << endl;
 				}
 			}
 			else
@@ -39,20 +40,22 @@ string checkAcc(string username, string password)
 		}
 		myfile.close();
 	}
-	return "DEF";
+	cout << "Please try to loign again!" << endl;
+	return "invalidAccount";
 }
 
 int tokenize(string line, string* results, char delimiter) {
-	string help, help1 = line;
-	int counter = 0;
-	for (int i = 0; i < help1.size(); i++)
+	string help;
+	int counter = 0, count = 0;
+	for (int i = 0; i < line.size(); i++)
 	{
-		if (help1[i] == delimiter)
+		if (line[i] == delimiter)
 		{
 			results[counter++] = help;
 			help = "";
+			i++;
 		}
-		help[i] = help1[i];
+		help += line[i];
 	}
 	return counter;
 }
@@ -61,8 +64,11 @@ void manageAccounts()
 {
 	int choice;
 	ifstream myfile("acc.txt");
-	string tokens[150], help;
+	ofstream tmpFile("accTmp.txt");
+	string tokens[10], help;
+	char FilePath[_MAX_PATH],fileName[]="acc.txt";
 	int counter = 0, checkCounter = 0;
+	string line;
 
 	cout << "___________________________________________________" << endl;
 	cout << endl;
@@ -106,22 +112,106 @@ void manageAccounts()
 				}
 			}
 		}
+		break;
 	case 2:
-		/*cout << "Name of user: ";
-		cin >> help;*/
+		cout << "Name of user: ";
+		cin >> help;
 		if (myfile.is_open())
 		{
 			string line;
 			while (!myfile.eof())
 			{
-				string line((istreambuf_iterator<char>(myfile)),
-					(istreambuf_iterator<char>()));
-				tokenize(line, tokens, ',');
-				cout << tokens;
+				getline(myfile, line);
+				//string line((istreambuf_iterator<char>(myfile)),
+					//(istreambuf_iterator<char>()));
+				if (line != "") {
+					tokenize(line, tokens, ',');
+					if (help != tokens[0]) {
+						if (tmpFile.is_open())
+						{
+							counter = 2;
+							tmpFile << tokens[counter] <<","<< tokens[counter--] <<","<< tokens[counter--]<<","<<endl;
+						}
+					}
+				}
+				else {
+				}
+			}
+			myfile.close();
+			if (remove("acc.txt") != 0) {
+				cerr << "A wild error appeared: ";
+			}
+			else {
+				cout << "Deleting account 50% done" << endl;
+			}
+			tmpFile.close();
+			if (rename("accTmp.txt", "acc.txt")!=0)
+			{
+				cerr<<"A wild error appeared : ";
+			}
+			else
+			{
+				cout << "Deleting account done!!!";
 			}
 		}
+		break;
 	case 3:
 
+		break;
+	case 4:
+		cout << "Name of user: ";
+		cin >> help;
+		if (myfile.is_open())
+		{
+			string line;
+			while (!myfile.eof())
+			{
+				getline(myfile, line);
+				//string line((istreambuf_iterator<char>(myfile)),
+					//(istreambuf_iterator<char>()));
+				if (line != "") {
+					tokenize(line, tokens, ',');
+					if (help != tokens[0]) {
+						if (tmpFile.is_open())
+						{
+
+							tmpFile << tokens[0] << "," << tokens[1] << "," << tokens[2] << "," << endl;
+						}
+					}
+					else
+					{
+						if (tokens[2]=="1")
+						{
+							counter = 2;
+							tmpFile << tokens[0] << "," << tokens[1] << "," << "0" << "," << endl;
+						}
+						else
+						{
+							counter = 2;
+							tmpFile << tokens[0] << "," << tokens[1] << "," << "1" << "," << endl;
+						}
+					}
+				}
+				else {
+				}
+			}
+			myfile.close();
+			if (remove("acc.txt") != 0) {
+				cerr << "A wild error appeared: ";
+			}
+			else {
+				cout << "Adding/Removing Admin 50% done!" << endl;
+			}
+			tmpFile.close();
+			if (rename("accTmp.txt", "acc.txt") != 0)
+			{
+				cerr << "A wild error appeared : ";
+			}
+			else
+			{
+				cout << "Adding/Removing Admin done!!!!";
+			}
+		}
 		break;
 	default:
 		break;
@@ -145,7 +235,7 @@ void writeDataIntoFile(ITEM newItem, int& itemCount)
 	data.open("items.txt", ios::app);
 
 	data << newItem.id << "|" << newItem.itemName << "|" << newItem.price << "|" << newItem.seller << "|" << newItem.description << "|" << 0 << "|" << endl;
-	
+
 	data.close();
 
 }
@@ -187,12 +277,12 @@ int getItemIndexById(ITEM* items, int& itemCount, int id)
 	return NULL;
 }
 
-void updateItem(ITEM* items, ITEM newItem, int& itemCount, int& maxId) {
+void updateItem(ITEM * items, ITEM newItem, int& itemCount, int& maxId) {
 	int index = getItemIndexById(items, itemCount, maxId);
 	items[index] = newItem;
 }
 
-void deleteItem(ITEM* items, int& itemCount, int id) {
+void deleteItem(ITEM * items, int& itemCount, int id) {
 
 	int index = getItemIndexById(items, itemCount, id);
 	for (int i = index; i < itemCount - 1; i++)
@@ -202,13 +292,13 @@ void deleteItem(ITEM* items, int& itemCount, int id) {
 	itemCount--;
 }
 
-ITEM getItemById(ITEM* items, int& itemCount, int id)
+ITEM getItemById(ITEM * items, int& itemCount, int id)
 {
 	int index = getItemIndexById(items, itemCount, id);
 	return items[index];
 }
 
-int inputDataInArray(ITEM* items)
+int inputDataInArray(ITEM * items)
 {
 	fstream data;
 	data.open("items.txt");
@@ -239,26 +329,26 @@ int inputDataInArray(ITEM* items)
 	return counter;
 }
 
-void initItemsInArray(ITEM* items, int& itemCount, int& maxId)
-{
-	insertItemInArray(items, itemCount, { "Gosho", "Bathroom tiles", 12.35, "the price is for m / sq" }, maxId);
-	insertItemInArray(items, itemCount, { "Alex", "Mouse Pad", 21.45, "35x45" }, maxId);
-	insertItemInArray(items, itemCount, { "Pesho", "LG TV", 769.99, "42 inches " }, maxId);
-	insertItemInArray(items, itemCount, { "Penka", "T-Shirt", 9.99, "XL size " }, maxId);
-	insertItemInArray(items, itemCount, { "Nelina", "White Mercedes", 6829, "Year of manufacture: 1997 " }, maxId);
-	insertItemInArray(items, itemCount, { "Milko", "Chickens", 20, "One chicken- 20 bgn " }, maxId);
-	insertItemInArray(items, itemCount, { "John", "Fridge", 178, "2x1" }, maxId);
-	insertItemInArray(items, itemCount, { "Miroslav", "Leather", 25, "25 bgn for 1 meter" }, maxId);
-	insertItemInArray(items, itemCount, { "Ivan", "Turkeys", 35, "35 bgn for 1 turkey" }, maxId);
-	insertItemInArray(items, itemCount, { "Martin", "Pillow", 15, "15 bgn for 1 pillow" }, maxId);
-}
+//void initItemsInArray(ITEM * items, int& itemCount, int& maxId)
+//{
+//	insertItemInArray(items, itemCount, { "Gosho", "Bathroom tiles", 12.35, "the price is for m / sq" }, maxId);
+//	insertItemInArray(items, itemCount, { "Alex", "Mouse Pad", 21.45, "35x45" }, maxId);
+//	insertItemInArray(items, itemCount, { "Pesho", "LG TV", 769.99, "42 inches " }, maxId);
+//	insertItemInArray(items, itemCount, { "Penka", "T-Shirt", 9.99, "XL size " }, maxId);
+//	insertItemInArray(items, itemCount, { "Nelina", "White Mercedes", 6829, "Year of manufacture: 1997 " }, maxId);
+//	insertItemInArray(items, itemCount, { "Milko", "Chickens", 20, "One chicken- 20 bgn " }, maxId);
+//	insertItemInArray(items, itemCount, { "John", "Fridge", 178, "2x1" }, maxId);
+//	insertItemInArray(items, itemCount, { "Miroslav", "Leather", 25, "25 bgn for 1 meter" }, maxId);
+//	insertItemInArray(items, itemCount, { "Ivan", "Turkeys", 35, "35 bgn for 1 turkey" }, maxId);
+//	insertItemInArray(items, itemCount, { "Martin", "Pillow", 15, "15 bgn for 1 pillow" }, maxId);
+//}
 
 /* Functions for managing items */
 
 
 /* Menus */
 
-void showAlloffers(ITEM* items, int& itemCount)
+void showAlloffers(ITEM * items, int& itemCount)
 {
 
 	for (int i = 0; i < itemCount; i++)
@@ -275,26 +365,29 @@ void showAlloffers(ITEM* items, int& itemCount)
 	}
 }
 
-void addOfferMenu(ITEM* items, int& itemCount, int& maxId)
+void addOfferMenu(ITEM * items, int& itemCount, int& maxId)
 {
 	ITEM newItem;
 	cin.ignore();
 	cout << "\n Enter Item Name: ";
-	getline(cin,newItem.itemName);
+	getline(cin, newItem.itemName);
 	cout << "Enter Price: ";
 	cin >> newItem.price;
 	cin.ignore();
 	cout << "Enter Your Name: ";
-	getline(cin,newItem.seller);
+	getline(cin, newItem.seller);
 	cout << "Enter More Information About The Product: ";
-	getline(cin,newItem.description);
+	getline(cin, newItem.description);
 
 	insertItemInArray(items, itemCount, newItem, maxId);
 }
 
-bool showItemsMenu(ITEM* items, int& itemCount,int& maxId)
+bool showItemsMenu(ITEM * items, int& itemCount, int& maxId)
 {
 	char input;
+	//int itemCount;
+	//ITEM items[200];
+	//itemCount = inputDataInArray(items);
 
 	cout << "___" << endl;
 	cout << endl;
@@ -317,8 +410,8 @@ bool showItemsMenu(ITEM* items, int& itemCount,int& maxId)
 		break;
 	case '2': return true;
 		break;
-		case '3':
-		addOfferMenu(items,itemCount,maxId);
+	case '3':
+		addOfferMenu(items, itemCount, maxId);
 		return true;
 		break;
 	case '9': return false;
@@ -329,7 +422,7 @@ bool showItemsMenu(ITEM* items, int& itemCount,int& maxId)
 		cin >> input;
 		cout << endl;
 	}
-		   break;
+			 break;
 	}
 	return true;
 }
@@ -367,7 +460,7 @@ bool adminMenu()
 		cout << endl;
 	}
 
-		   break;
+			  break;
 	}
 	return true;
 }
@@ -398,7 +491,7 @@ void Register() {
 }
 
 void login() {
-	string username, password;
+	string username, password, result = "invalidAccount";
 	char character;
 	cout << "Do you have existing account[Y/N]: ";
 	cin >> character;
@@ -413,18 +506,25 @@ void login() {
 
 	if (character == 'Y')
 	{
-		cout << "___________________________________________________" << endl;
-		cout << endl;
-		cout << "               |===== LOGIN =====|\n\n" << endl;
-		cout << "Username: ";
-		cin >> username;
-		cout << "Password: ";
-		cin >> password;
-		cout << endl;
-		cout << checkAcc(username, password);
+		while (result == "invalidAccount")
+		{
+			cout << "___________________________________________________" << endl;
+			cout << endl;
+			cout << "               |===== LOGIN =====|\n\n" << endl;
+			cout << "Username: ";
+			cin >> username;
+			cout << "Password: ";
+			cin >> password;
+			cout << endl;
+			result = checkAcc(username, password);
+		}
 		if (checkAcc(username, password) == "1")
 		{
-
+			adminMenu();
+		}
+		else
+		{
+			//showItemsMenu();
 		}
 	}
 	else if (character == 'N')
@@ -434,7 +534,7 @@ void login() {
 	}
 }
 
-bool Menu(ITEM* items, int& itemCount,int &maxId) {
+bool Menu(ITEM * items, int& itemCount, int& maxId) {
 	char input;
 	bool whileCheck = true;
 
@@ -454,7 +554,7 @@ bool Menu(ITEM* items, int& itemCount,int &maxId) {
 		switch (input)
 		{
 		case 'k':
-			showItemsMenu(items,itemCount,maxId);
+			showItemsMenu(items, itemCount, maxId);
 			return false;
 			break;
 		case '1':
@@ -477,7 +577,7 @@ bool Menu(ITEM* items, int& itemCount,int &maxId) {
 			cin >> input;
 			cout << endl;
 		}
-			   break;
+				 break;
 		}
 	}
 	return true;
@@ -497,7 +597,7 @@ int main()
 	cin.ignore();
 	getline(cin, nov.seller);
 	getline(cin, nov.description);
-	
+
 	insertItemInArray(newItem, itemCount, nov, maxId);
 	//initItemsInArray(newItem, itemCount,maxId);
 	writeDataIntoFile(newItem, itemCount);
